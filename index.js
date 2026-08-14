91b1169663fdfcedda228b08cef21bad3b153d55
Silver <yellowochregas@gmail.com>
2026-08-14 09:36:03 +0100

    added an index file

---
+++ index.js
@@ -0,0 +1,5 @@
+const name = "Victor Usese";
+const age = 28;
+const favouriteProgrammingLanguage = "JavaScript";
+
+console.log(`My name is ${name}, I am ${age} years old and my favourite programming language is none but ${favouriteProgrammingLanguage}.`); 
